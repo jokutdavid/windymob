6e2766810a7946812be2877ef4d78a263f5665c5
David Jokuti <jokutda@whs.school.nz>
2025-05-23 21:10:43 +1200

    Improves frame rate warning message
    Updates the frame rate warning to display the estimated frames per second, instead of delta time, and adjusts the threshold to 55 fps.

--- main.gd
+++ main.gd
@@ -3,6 +3,5 @@ extends Node2D
 @export var row_cols = Vector2i.ZERO
 
 func _process(delta):
-	if delta > 1.0 / 50:
-		print(str(delta) + ":" + str(1.0 / 50))
-		print("Sub 50 fps")
+	if delta > 1.0 / 55:
+		print("Warning! " + str(int(1 / delta)) + "fps")
