8dc651f2fd3a3d9839fa70c0693244edbafe346b
David Jokuti <jokutda@whs.school.nz>
2025-05-27 07:43:27 +1200

    Renames "Creatures" node to "People"
    Renames the "Creatures" node to "People" for better clarity.
    
    Adds a "none" tile type.
    
    Refactors tile sprite updating to only occur when the tile type changes.
    
    Renames `randomize_text_rotations` to `randomize_texture_rotations`.

--- scripts/tile.gd
+++ scripts/tile.gd
@@ -3,12 +3,15 @@ extends Node2D
 @export var type = tile_type.dirt
 var id : Vector2
 
+var old_type = tile_type.none
+
 enum tile_type {
+	none,
 	dirt,
 	shallow_water,
 }
 
-func randomize_text_rotations():
+func randomize_texture_rotations():
 	match randi_range(0, 3):
 		0:
 			$Area2D/TextureRect.rotation_degrees = 0
@@ -32,8 +35,10 @@ func update_sprite():
 	texture_rect.texture = texture
 
 func _ready():
-	randomize_text_rotations()
+	randomize_texture_rotations()
 
 func _process(delta):
-	update_sprite()
+	if type != old_type:
+		old_type = type
+		update_sprite()
 	
